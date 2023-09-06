4017e943cbfba1b09394c276e6f59304944859a1
jerojash <javiereduardorh@gmail.com>
2023-09-06 19:24:22 -0400

    Add DTO registerUser

---
+++ src/user/application/dto/registerUser.dto.ts
@@ -0,0 +1,11 @@
+
+export class UserDTO {
+
+    constructor(public firstName: string,
+        public lastName: string, 
+        public email: string, 
+        public age: number, 
+        public password: string
+        ){};
+
+  }
