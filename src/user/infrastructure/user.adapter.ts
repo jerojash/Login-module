d74468346b14a1fa2ffa4a611f2d9aae0c8bfb23
jerojash <javiereduardorh@gmail.com>
2023-09-06 20:53:10 -0400

    Add UserAdapter

---
+++ src/user/infrastructure/user.adapter.ts
@@ -0,0 +1,26 @@
+import { Either } from "../../generics/Either";
+import { registerUserDTO } from "../application/dto/registerUser.dto";
+import { User } from "../domain/User";
+import { IUser } from "../domain/repository/IUser";
+import UserModel from "./model/user.schema";
+
+export class UserAdapterRepository implements IUser<registerUserDTO>{
+    async registerUser(user: User): Promise<Either<Error,registerUserDTO>>{
+        
+        
+        const UserEntity = new registerUserDTO(user.getUsername().getUsername(),
+                            user.getPassword().getPassword(),
+                            user.getFullName().getFirstName(),
+                            user.getFullName().getLastName(),
+                            user.getEmail().getEmail());
+        try {
+            const userBD = await UserModel.create(UserEntity);
+
+            return Either.makeRight<Error,registerUserDTO>(UserEntity);
+        } catch (error) {
+            return Either.makeLeft<Error, registerUserDTO>(new Error(`Ocurrio un error inesperado: ${error}`));
+        }
+        
+    }
+
+}
